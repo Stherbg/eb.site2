8c4ac746aac9442efa20aba0e7f9c4eee8c2dca8
Stherbg <mariastellasaldanhabeserra@gmail.com>
2026-07-10 13:30:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-document.getElementById("comprar").onclick = function() {
-    window.open("https://discord.com/channels/1518657693999562792/1518680495817490462", "_blank");
+function suporte(){
+window.open("https://discord.gg/pWtJdpfDK","_blank");
 }
 
-document.getElementById("suporte").onclick = function() {
-    window.open("https://discord.gg/VBb5aPe9aY", "_blank");
+function comprar(){
+window.open("https://discord.com/channels/1518657693999562792/1518680495817490462/1524547209851306086","blank_");
 }
 
-document.getElementById("alistamento").onclick = function() {
-    window.open("https://www.roblox.com/pt/communities/388942212/CIE-Centro-de-Intelig-ncia-do-Ex-rcito#!/about", "_blank");
+function comunidade(){
+window.open("https://www.roblox.com/pt/communities/388942212/CIE-Centro-de-Intelig-ncia-do-Ex-rcito#!/about","_blank");
 }
