352b5c1cddc83859f5117125acfdb377a3c9dd21
Stherbg <mariastellasaldanhabeserra@gmail.com>
2026-07-10 13:04:13 -0300

    Criar script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("comprar").onclick = function() {
+    window.open("https://discord.com/channels/1518657693999562792/1518680495817490462", "_blank");
+}
+
+document.getElementById("suporte").onclick = function() {
+    window.open("https://discord.gg/VBb5aPe9aY", "_blank");
+}
+
+document.getElementById("alistamento").onclick = function() {
+    window.open("https://www.roblox.com/pt/communities/388942212/CIE-Centro-de-Intelig-ncia-do-Ex-rcito#!/about", "_blank");
+}
